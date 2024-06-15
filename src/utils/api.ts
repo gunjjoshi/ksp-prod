cb465218c149abecceb11191543499c947892ae5
GUNJ JOSHI <gunjjoshi8372@gmail.com>
2024-06-15 12:02:01 +0530

    update prod url

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 let urls = {
     test: `http://localhost:3000`,
     development: `http://localhost:3000`,
-    production: `https://knowledge-sharing-platform.vercel.app`,
+    production: `https://ksp-prod.vercel.app`,
 }
 
 export const api = axios.create({
