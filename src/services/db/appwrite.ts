eb37306c625b4098c8331cedfeab8cbeacd3bde4
Gunj Joshi <gunjjoshi8372@gmail.com>
2024-09-22 11:57:03 +0530

    Update appwrite.ts

--- src/services/db/appwrite.ts
+++ src/services/db/appwrite.ts
@@ -5,7 +5,6 @@ const client = new Client();
 client
 .setEndpoint('https://cloud.appwrite.io/v1')
 .setProject('65ca771866bb9870c919')
-    // .setKey('your-appwrite-api-key');
 
 export default client;
 export const storage = new Storage(client)
